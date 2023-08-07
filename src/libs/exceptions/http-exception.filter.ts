a301d59a61c778de1906902c944baa460983af9f
김효민 <hyomin@gimhyomins-MacBook-Pro.local>
2023-08-07 09:35:46 +0900

    ✨ feat: send error message to slack

--- src/libs/exceptions/http-exception.filter.ts
+++ src/libs/exceptions/http-exception.filter.ts
@@ -1,16 +1,23 @@
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common'
+import { ExceptionFilter, Catch, ArgumentsHost, HttpStatus, HttpException } from '@nestjs/common'
 import { Request, Response } from 'express'
 import got from 'got'
 
-@Catch(HttpException)
+@Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+  async catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToHttp()
     const response = ctx.getResponse<Response>()
     const request = ctx.getRequest<Request>()
-    const status = exception.getStatus()
-    const errorCode = exception.getResponse()['error']
-    console.log(exception)
+    const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR
+
+    let errorCode: string = null
+    if (status < 500) {
+      errorCode = exception.getResponse()['error']
+    }
+
+    got.post('https://hooks.slack.com/services/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', {
+      json: { text: JSON.stringify({ ...exception }) },
+    })
 
     response.status(status).json({
       errorCode: Array.isArray(errorCode) ? [...errorCode] : [errorCode],
