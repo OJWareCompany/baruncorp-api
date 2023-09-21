68dc83a3ef38c8b541ee8590a65b73f5a3dda972
김효민 <hyomin@gimhyomins-MacBook-Pro.local>
2023-09-21 17:24:17 +0900

    🐞 fix: fixed user name when create job note

--- src/modules/ordered-job-note/commands/create-job-note/create-job-note.service.ts
+++ src/modules/ordered-job-note/commands/create-job-note/create-job-note.service.ts
@@ -1,18 +1,27 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
 import { PrismaService } from '../../../database/prisma.service'
+import { UserNotFoundException } from '../../../users/user.error'
+import UserMapper from '../../../users/user.mapper'
 import { JobNoteEntity } from '../../domain/job-note.entity'
-import { CreateJobNoteCommand } from './create-job-note.command'
 import { JobNoteMapper } from '../../job-note.mapper'
+import { CreateJobNoteCommand } from './create-job-note.command'
 
 @CommandHandler(CreateJobNoteCommand)
 export class CreateJobNoteService implements ICommandHandler {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  constructor(private readonly prismaService: PrismaService, private readonly jobNoteMapper: JobNoteMapper) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly jobNoteMapper: JobNoteMapper,
+    private readonly userMapper: UserMapper,
+  ) {}
   async execute(command: CreateJobNoteCommand): Promise<{ id: string }> {
+    const clientUser = await this.prismaService.users.findUnique({ where: { id: command.commenterUserId } })
+    if (!clientUser) throw new UserNotFoundException()
+    const clientUserEntity = this.userMapper.toDomain(clientUser)
     const entity = JobNoteEntity.create({
       jobId: command.jobId,
       content: command.content,
-      commenterName: command.commenterUserId,
+      commenterName: clientUserEntity.getProps().userName.getFullName(),
       commenterUserId: command.commenterUserId,
     })
 
