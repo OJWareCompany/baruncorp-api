2414ddb0fb24927f782ed9ee726a61b6358c46d1
김효민 <hyomin@gimhyomins-MacBook-Pro.local>
2023-09-25 16:39:52 +0900

    🐳 chore: cqrs code generator (handler return type)

--- cqrs-generator/query-format/query-handler.format.mjs
+++ cqrs-generator/query-format/query-handler.format.mjs
@@ -3,6 +3,7 @@ import { toCamelCase, toPascalCase } from '../util/string-convertor.mjs'
 export function getQueryHandlerContent(folderName, domainName) {
   return `import { NotFoundException } from '@nestjs/common'
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs'
+import { ${toPascalCase(domainName)}s } from '@prisma/client'
 import { initialize } from '../../../../libs/utils/constructor-initializer'
 import { PrismaService } from '../../../database/prisma.service'
 
@@ -17,8 +18,8 @@ export class ${toPascalCase(folderName)}Query {
 export class ${toPascalCase(folderName)}QueryHandler implements IQueryHandler {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async execute(query: ${toPascalCase(folderName)}Query): Promise<any> {
-    const result = await this.prismaService.${toCamelCase(domainName)}.findUnique({ where: { id: query.${toCamelCase(
+  async execute(query: ${toPascalCase(folderName)}Query): Promise<${toPascalCase(domainName)}s> {
+    const result = await this.prismaService.${toCamelCase(domainName)}s.findUnique({ where: { id: query.${toCamelCase(
     domainName,
   )}Id } })
     if (!result) throw new NotFoundException()
