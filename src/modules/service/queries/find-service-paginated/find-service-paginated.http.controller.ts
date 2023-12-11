3d7605fc327a0be7ef693bb23bbbf3ca0fe43ca0
김효민 <hyomin@gimhyomins-MacBook-Pro.local>
2023-12-11 17:55:06 +0900

    🦄 refactor: find service:

--- src/modules/service/queries/find-service-paginated/find-service-paginated.http.controller.ts
+++ src/modules/service/queries/find-service-paginated/find-service-paginated.http.controller.ts
@@ -6,10 +6,12 @@ import { PaginatedQueryRequestDto } from '../../../../libs/api/paginated-query.r
 import { ServicePaginatedResponseDto } from '../../dtos/service.paginated.response.dto'
 import { FindServicePaginatedQuery } from './find-service-paginated.query-handler'
 import { ServicePricingTypeEnum } from '../../domain/service.type'
+import { ServiceMapper } from '../../service.mapper'
+import { ServiceResponseDto } from '../../dtos/service.response.dto'
 
 @Controller('services')
 export class FindServicePaginatedHttpController {
-  constructor(private readonly queryBus: QueryBus) {}
+  constructor(private readonly queryBus: QueryBus, serviceMapper: ServiceMapper) {}
 
   @Get()
   async get(@Query() queryParams: PaginatedQueryRequestDto): Promise<ServicePaginatedResponseDto> {
@@ -18,38 +20,11 @@ export class FindServicePaginatedHttpController {
       limit: queryParams.limit,
     })
 
-    const result: Paginated<
-      Service & { tasks: Tasks[]; commercialStandardPricingTiers: CommercialStandardPricingTiers[] }
-    > = await this.queryBus.execute(command)
+    const result: Paginated<ServiceResponseDto> = await this.queryBus.execute(command)
 
     return new ServicePaginatedResponseDto({
       ...result,
-      items: result.items.map((item) => ({
-        id: item.id,
-        name: item.name,
-        billingCode: item.billingCode,
-        pricingType: item.fixedPrice ? ServicePricingTypeEnum.fixed : ServicePricingTypeEnum.standard,
-        standardPricing: {
-          residentialPrice: item.basePrice ? Number(item.basePrice) : null,
-          residentialGmPrice: item.gmPrice ? Number(item.gmPrice) : null,
-          residentialRevisionPrice: item.revisionPrice ? Number(item.revisionPrice) : null,
-          residentialRevisionGmPrice: item.revisionGmPrice ? Number(item.revisionGmPrice) : null,
-          commercialRevisionCostPerUnit: item.commercialRevisionCostPerUnit
-            ? Number(item.commercialRevisionCostPerUnit)
-            : null,
-          commercialRevisionMinutesPerUnit: item.commercialRevisionMinutesPerUnit
-            ? Number(item.commercialRevisionMinutesPerUnit)
-            : null,
-          commercialNewServiceTiers: item.commercialStandardPricingTiers.map((tier) => ({
-            startingPoint: Number(tier.startingPoint),
-            finishingPoint: Number(tier.finishingPoint),
-            price: Number(tier.price),
-            gmPrice: Number(tier.gmPrice),
-          })),
-        },
-        fixedPrice: item.fixedPrice ? Number(item.fixedPrice) : null,
-        relatedTasks: item.tasks,
-      })),
+      items: result.items,
     })
   }
 }
--- src/modules/service/queries/find-service-paginated/find-service-paginated.query-handler.ts
+++ src/modules/service/queries/find-service-paginated/find-service-paginated.query-handler.ts
@@ -4,6 +4,9 @@ import { initialize } from '../../../../libs/utils/constructor-initializer'
 import { PaginatedParams, PaginatedQueryBase } from '../../../../libs/ddd/query.base'
 import { Paginated } from '../../../../libs/ddd/repository.port'
 import { PrismaService } from '../../../database/prisma.service'
+import { ServiceMapper } from '../../service.mapper'
+import { ServiceEntity } from '../../domain/service.entity'
+import { ServiceResponseDto } from '../../dtos/service.response.dto'
 
 export class FindServicePaginatedQuery extends PaginatedQueryBase {
   constructor(props: PaginatedParams<FindServicePaginatedQuery>) {
@@ -14,24 +17,29 @@ export class FindServicePaginatedQuery extends PaginatedQueryBase {
 
 @QueryHandler(FindServicePaginatedQuery)
 export class FindServicePaginatedQueryHandler implements IQueryHandler {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly prismaService: PrismaService, private readonly serviceMapper: ServiceMapper) {}
 
-  async execute(
-    query: FindServicePaginatedQuery,
-  ): Promise<
-    Paginated<Service & { tasks: Tasks[]; commercialStandardPricingTiers: CommercialStandardPricingTiers[] }>
-  > {
+  async execute(query: FindServicePaginatedQuery): Promise<Paginated<ServiceResponseDto>> {
     const result = await this.prismaService.service.findMany({
       include: { tasks: true, commercialStandardPricingTiers: true },
       skip: query.offset,
       take: query.limit,
     })
+    const entities = result.map((record) => {
+      return this.serviceMapper.toDomain({
+        service: record,
+        commercialStandardPricingTiers: record.commercialStandardPricingTiers,
+        tasks: record.tasks,
+      })
+    })
+    const response = entities.map(this.serviceMapper.toResponse)
+
     const totalCount = await this.prismaService.service.count()
     return new Paginated({
       page: query.page,
       pageSize: query.limit,
       totalCount: totalCount,
-      items: result,
+      items: response,
     })
   }
 }
