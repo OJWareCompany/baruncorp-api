116cf063c416b7ccafba7223b3cf87ca4d18036e
김효민 <hyomin@gimhyomins-MacBook-Pro.local>
2023-12-26 02:14:06 +0900

    ✨ feat: find unregistered users for task

--- src/modules/task/queries/find-unregistered-users-for-task/find-unregistered-users-for-task.http.controller.ts
+++ src/modules/task/queries/find-unregistered-users-for-task/find-unregistered-users-for-task.http.controller.ts
@@ -4,6 +4,8 @@ import { FindUnregisteredUsersForTaskPaginatedQuery } from './find-unregistered-
 import { PaginatedQueryRequestDto } from '../../../../libs/api/paginated-query.request.dto'
 import { FindUnregisteredUsersForTaskPaginatedRequestParamDto } from './find-unregistered-users-for-task.request.dto'
 import { UnregisteredUserForTaskPaginatedResponseDto } from '../../dtos/unregistered-user-for-task.paginated.response'
+import { Users } from '@prisma/client'
+import { Paginated } from '../../../../libs/ddd/repository.port'
 
 @Controller('tasks')
 export class FindUnregisteredUsersForTaskHttpController {
@@ -15,26 +17,18 @@ export class FindUnregisteredUsersForTaskHttpController {
     @Query() queryParams: PaginatedQueryRequestDto,
   ): Promise<UnregisteredUserForTaskPaginatedResponseDto> {
     const command = new FindUnregisteredUsersForTaskPaginatedQuery({ taskId: param.taskId })
+    const result: Paginated<Users> = await this.queryBus.execute(command)
 
-    // const result: Paginated<Tasks> = await this.queryBus.execute(command)
     return new UnregisteredUserForTaskPaginatedResponseDto({
-      page: 1,
-      pageSize: 1,
-      totalCount: 1,
-      items: [
-        {
-          userId: 'asdasd',
-          userName: 'chris kim',
-        },
-        {
-          userId: 'a2eda3w2',
-          userName: 'hyomin kim',
-        },
-        {
-          userId: 'q3daa2d21',
-          userName: 'yunwoo ji',
-        },
-      ],
+      page: result.page,
+      pageSize: result.pageSize,
+      totalCount: result.totalCount,
+      items: result.items.map((user) => {
+        return {
+          userId: user.id,
+          userName: user.firstName + ' ' + user.lastName,
+        }
+      }),
     })
   }
 }
--- src/modules/task/queries/find-unregistered-users-for-task/find-unregistered-users-for-task.query-handler.ts
+++ src/modules/task/queries/find-unregistered-users-for-task/find-unregistered-users-for-task.query-handler.ts
@@ -1,6 +1,6 @@
 import { NotFoundException } from '@nestjs/common'
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs'
-import { Tasks } from '@prisma/client'
+import { Tasks, Users } from '@prisma/client'
 import { PaginatedParams, PaginatedQueryBase } from '../../../../libs/ddd/query.base'
 import { initialize } from '../../../../libs/utils/constructor-initializer'
 import { Paginated } from '../../../../libs/ddd/repository.port'
@@ -18,17 +18,25 @@ export class FindUnregisteredUsersForTaskPaginatedQuery extends PaginatedQueryBa
 export class FindUnregisteredUsersForTaskPaginatedQueryHandler implements IQueryHandler {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async execute(query: FindUnregisteredUsersForTaskPaginatedQuery): Promise<Paginated<any>> {
-    // const result = await this.prismaService.tasks.findMany({
-    //   skip: query.offset,
-    //   take: query.limit,
-    // })
-    // const totalCount = await this.prismaService.tasks.count()
-    return new Paginated<Tasks>({
-      items: [],
+  async execute(query: FindUnregisteredUsersForTaskPaginatedQuery): Promise<Paginated<Users>> {
+    const userTasks = await this.prismaService.userAvailableTasks.findMany({ where: { taskId: query.taskId } })
+    const users = await this.prismaService.users.findMany({
+      skip: query.offset,
+      take: query.limit,
+      where: {
+        id: { notIn: userTasks.map((task) => task.userId) },
+      },
+    })
+    const totalCount = await this.prismaService.tasks.count({
+      where: {
+        id: { notIn: userTasks.map((task) => task.userId) },
+      },
+    })
+    return new Paginated<Users>({
       page: query.page,
       pageSize: query.limit,
-      totalCount: 1,
+      totalCount,
+      items: users,
     })
   }
 }
