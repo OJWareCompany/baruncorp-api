b7ea11856b8ae18cb21aaae14e043d57722ac355
김효민 <hyomin@gimhyomins-MacBook-Pro.local>
2024-01-03 16:52:34 +0900

    🐞 fix: used wrong organization id in query

--- src/modules/assigned-task/application/event-handlers/update-cost-when-task-is-assigned.domain-event-handler.ts
+++ src/modules/assigned-task/application/event-handlers/update-cost-when-task-is-assigned.domain-event-handler.ts
@@ -9,6 +9,8 @@ import { OrderedServiceRepositoryPort } from '../../../ordered-service/database/
 import { EXPENSE_PRICING_REPOSITORY } from '../../../expense-pricing/expense-pricing.di-token'
 import { ExpensePricingRepositoryPort } from '../../../expense-pricing/database/expense-pricing.repository.port'
 import { AssignedTaskAssignedDomainEvent } from '../../domain/events/assigned-task-assigned.domain-event'
+import { USER_REPOSITORY } from '../../../users/user.di-tokens'
+import { UserRepositoryPort } from '../../../users/database/user.repository.port'
 
 export class UpdateCostWhenTaskIsAssignedDomainEventHandler {
   constructor(
@@ -18,14 +20,17 @@ export class UpdateCostWhenTaskIsAssignedDomainEventHandler {
     @Inject(ORDERED_SERVICE_REPOSITORY) private readonly orderedServiceRepo: OrderedServiceRepositoryPort,
     // @ts-ignore
     @Inject(EXPENSE_PRICING_REPOSITORY) private readonly expensePricingRepo: ExpensePricingRepositoryPort,
+    // @ts-ignore
+    @Inject(USER_REPOSITORY) private readonly userRepo: UserRepositoryPort,
     private readonly calculateVendorCostDomainService: CalculateVendorCostDomainService,
   ) {}
   @OnEvent([AssignedTaskAssignedDomainEvent.name])
   async handle(event: AssignedTaskAssignedDomainEvent) {
     const assignedTask = await this.assignedTaskRepo.findOneOrThrow(event.aggregateId)
     const orderedService = await this.orderedServiceRepo.findOneOrThrow(assignedTask.orderedServiceId)
+    const user = await this.userRepo.findOneByIdOrThrow(event.assigneeUserId)
     const expensePricing = await this.expensePricingRepo.findOneOrThrow(
-      assignedTask.organizationId,
+      user.getProps().organization.id,
       assignedTask.taskId,
     )
     assignedTask.updateCost(this.calculateVendorCostDomainService, expensePricing, orderedService)
