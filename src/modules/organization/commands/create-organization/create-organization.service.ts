737791c372b56073ac7f8815271a684a1e358683
ojsangwon <sangwon@ojware.com>
2024-01-18 22:07:56 +0900

    ✨ feat: integrate with file server api (create google shared drive)

--- src/modules/organization/commands/create-organization/create-organization.service.ts
+++ src/modules/organization/commands/create-organization/create-organization.service.ts
@@ -1,11 +1,14 @@
+import { PrismaService } from './../../../database/prisma.service'
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import { ConflictException, Inject } from '@nestjs/common'
+import { Inject } from '@nestjs/common'
 import { OrganizationEntity } from '../../domain/organization.entity'
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
 import { CreateOrganizationCommand } from './create-organization.command'
 import { ORGANIZATION_REPOSITORY } from '../../organization.di-token'
 import { OrganizationRepositoryPort } from '../../database/organization.repository.port'
 import { OrganizationConflictException } from '../../domain/organization.error'
+import { FilesystemApiService } from '../../../filesystem/infra/filesystem.api.service'
+import { OrganizationMapper } from '../../organization.mapper'
 
 // TODO: remove id field!
 
@@ -14,12 +17,22 @@ export class CreateOrganizationService implements ICommandHandler {
   constructor(
     // @ts-ignore
     @Inject(ORGANIZATION_REPOSITORY) private readonly organizationRepository: OrganizationRepositoryPort,
+    private readonly prismaService: PrismaService,
+    private readonly filesystemApiService: FilesystemApiService,
+    private readonly organizationMapper: OrganizationMapper,
   ) {}
 
   async execute(command: CreateOrganizationCommand): Promise<{ id: string }> {
-    const organization = await this.organizationRepository.findOneByName(command.name)
-    if (organization) throw new OrganizationConflictException(command.name)
-    const entity = OrganizationEntity.create({
+    const organizationName = command.name
+    const organization = await this.organizationRepository.findOneByName(organizationName)
+    if (organization) throw new OrganizationConflictException(organizationName)
+
+    const createGoogleSharedDriveResponseData = await this.filesystemApiService.requestToCreateGoogleSharedDrive(
+      organizationName,
+    )
+
+    const { sharedDrive, residentialFolder, commercialFolder } = createGoogleSharedDriveResponseData
+    const organizationEntity = OrganizationEntity.create({
       name: command.name,
       phoneNumber: command.phoneNumber,
       // organizationType: command.organizationType,
@@ -36,11 +49,38 @@ export class CreateOrganizationService implements ICommandHandler {
       numberOfFreeRevisionCount: command.numberOfFreeRevisionCount,
       isVendor: command.isVendor,
     })
+    const organizationRecord = this.organizationMapper.toPersistence(organizationEntity)
+    const organizationId = organizationEntity.id
 
-    await this.organizationRepository.insertOrganization(entity)
+    try {
+      await this.prismaService.$transaction([
+        this.prismaService.organizations.create({ data: organizationRecord }),
+        this.prismaService.googleSharedDrive.create({
+          data: {
+            id: sharedDrive.id,
+            name: sharedDrive.name,
+            residentialFolderId: residentialFolder.id,
+            commercialFolderId: commercialFolder.id,
+            organizationId,
+          },
+        }),
+      ])
+    } catch (error) {
+      const itemIds = []
+      if (!residentialFolder.matchedExistingData) itemIds.push(residentialFolder.id)
+      if (!commercialFolder.matchedExistingData) itemIds.push(commercialFolder.id)
+      await this.filesystemApiService.requestToDeleteItemsInSharedDrive({
+        sharedDrive: {
+          id: sharedDrive.id,
+          delete: !sharedDrive.matchedExistingData,
+        },
+        itemIds,
+      })
+      throw error
+    }
 
     return {
-      id: entity.id,
+      id: organizationId,
     }
   }
 }
