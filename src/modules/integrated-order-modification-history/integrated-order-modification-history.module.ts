ad64b0b27ed75f5c13b90eaeea9144b626cad932
김효민 <hyomin@gimhyomins-MacBook-Pro.local>
2024-01-28 04:05:46 +0900

    🐞 fix: removed unused code

--- src/modules/integrated-order-modification-history/integrated-order-modification-history.module.ts
+++ src/modules/integrated-order-modification-history/integrated-order-modification-history.module.ts
@@ -7,7 +7,7 @@ import { FindIntegratedOrderModificationHistoryPaginatedHttpController } from '.
 import { FindIntegratedOrderModificationHistoryQueryHandler } from './queries/find-integrated-order-modification-history/find-integrated-order-modification-history.query-handler'
 import { FindIntegratedOrderModificationHistoryPaginatedQueryHandler } from './queries/find-integrated-order-modification-history-paginated/find-integrated-order-modification-history.paginated.query-handler'
 import { OrderModificationHistoryGenerator } from './domain/domain-services/order-modification-history-generator.domain-service'
-import { OrderModificationInterceptor } from './domain/domain-services/order-modification.interceptor'
+// import { OrderModificationInterceptor } from './domain/domain-services/order-modification.interceptor'
 import { UsersModule } from '../users/users.module'
 import { JobModule } from '../ordered-job/job.module'
 
@@ -24,7 +24,7 @@ const repositories: Provider[] = []
 const eventHandlers: Provider[] = []
 const mappers: Provider[] = [UserMapper]
 const domainServices: Provider[] = [OrderModificationHistoryGenerator]
-const interceptors: Provider[] = [OrderModificationInterceptor]
+// const interceptors: Provider[] = [OrderModificationInterceptor]
 @Module({
   imports: [CqrsModule, PrismaModule, forwardRef(() => UsersModule), forwardRef(() => JobModule)],
   providers: [
@@ -34,7 +34,7 @@ const interceptors: Provider[] = [OrderModificationInterceptor]
     ...repositories,
     ...mappers,
     ...domainServices,
-    ...interceptors,
+    // ...interceptors,
   ],
   controllers: [...httpControllers],
   exports: [...domainServices],
