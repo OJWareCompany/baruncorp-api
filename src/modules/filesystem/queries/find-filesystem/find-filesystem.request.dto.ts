47357dda4800e8dc42149bfd1e340665cd24c3d0
ojsangwon <sangwon@ojware.com>
2024-02-02 11:13:50 +0900

    🐳 chore: delete no need files

--- src/modules/filesystem/queries/find-filesystem/find-filesystem.request.dto.ts
+++
@@ -1,8 +0,0 @@
-// import { ApiProperty } from '@nestjs/swagger'
-// import { IsString } from 'class-validator'
-
-// export class FindFilesystemRequestDto {
-//   @ApiProperty({ default: '' })
-//   @IsString()
-//   readonly filesystemId: string
-// }
