d83c7b421db85bad03291bc773dfd7ed2e9c9061
김효민 <hyomin@gimhyomins-MacBook-Pro.local>
2024-03-11 22:30:47 +0900

    🐞 fix: fixed hands up api for auto assignment type

--- src/modules/users/commands/hands-up/hands-up.service.ts
+++ src/modules/users/commands/hands-up/hands-up.service.ts
@@ -11,6 +11,9 @@ import { UserRepositoryPort } from '../../database/user.repository.port'
 import { ASSIGNED_TASK_REPOSITORY } from '../../../assigned-task/assigned-task.di-token'
 import { AssignedTaskRepositoryPort } from '../../../assigned-task/database/assigned-task.repository.port'
 import { OrderModificationValidator } from '../../../ordered-job/domain/domain-services/order-modification-validator.domain-service'
+import { AutoAssignmentTypeEnum } from '../../../position/domain/position.type'
+import { AssignedTasks, UserAvailableTasks } from '@prisma/client'
+import { UserEntity } from '../../domain/user.entity'
 
 @CommandHandler(HandsUpCommand)
 export class HandsUpService implements ICommandHandler {
@@ -26,7 +29,9 @@ export class HandsUpService implements ICommandHandler {
     private readonly orderModificationValidator: OrderModificationValidator,
   ) {}
   async execute(command: HandsUpCommand): Promise<void> {
-    const availableTasks = await this.prismaService.userAvailableTasks.findMany({ where: { userId: command.userId } })
+    const availableTasks = await this.prismaService.userAvailableTasks.findMany({
+      where: { userId: command.userId, autoAssignmentType: { not: AutoAssignmentTypeEnum.none } },
+    })
     if (!availableTasks.length) throw new AvailableTaskNotFoundException()
     const user = await this.userRepo.findOneByIdOrThrow(command.userId)
     if (user.getProps().isHandRaisedForTask) throw new BadRequestException('already hand raised ', '77002')
@@ -41,32 +46,9 @@ export class HandsUpService implements ICommandHandler {
       orderBy: [{ is_expedited: 'desc' }, { created_at: 'asc' }],
     })
 
-    for (const pendingTask of pendingTasks) {
-      const task = await this.prismaService.tasks.findUnique({ where: { id: pendingTask.taskId } })
-      if (!task) continue // TODO: 관리자에게 알람?
-      if (task.license_type) {
-        const project = await this.prismaService.orderedProjects.findUnique({ where: { id: pendingTask.projectId } })
-        if (!project) continue // TODO: 관리자에게 알람?
-        const state = await this.prismaService.states.findFirst({ where: { geoId: project.stateId } })
-        if (!state) continue // TODO: 관리자에게 알람?
+    const hasAssigned = await this.assign(user, availableTasks, pendingTasks)
 
-        const userLicenses = await this.prismaService.userLicense.findMany({
-          where: {
-            type: task.license_type,
-            abbreviation: state.abbreviation,
-            userId: command.userId,
-          },
-        })
-        // 해당 라이센스가 없다면 다음 태스크 순회
-        if (!userLicenses.length) continue
-      }
-
-      // 할당한다.
-      const assignedTaskEntity = this.assignedTaskMapper.toDomain(pendingTask)
-      await assignedTaskEntity.assign(user, this.orderModificationValidator) // OK?
-      await this.assignedTaskRepo.update(assignedTaskEntity)
-      return
-    }
+    if (hasAssigned) return
 
     // 할당된 태스크가 없다면 손을 들고 종료
     await Promise.all(
@@ -87,5 +69,49 @@ export class HandsUpService implements ICommandHandler {
         isHandRaisedForTask: true,
       },
     })
+  }
+
+  async assign(
+    user: UserEntity,
+    availableTasks: UserAvailableTasks[],
+    pendingTasks: AssignedTasks[],
+  ): Promise<boolean> {
+    for (const pendingTask of pendingTasks) {
+      const task = await this.prismaService.tasks.findUnique({ where: { id: pendingTask.taskId } })
+      if (!task) continue // TODO: 관리자에게 알람?
+      const project = await this.prismaService.orderedProjects.findUnique({ where: { id: pendingTask.projectId } })
+      if (!project) continue // TODO: 관리자에게 알람?
+
+      // 자동할당 옵션 적용
+      const targetTask = availableTasks.find((at) => at.taskId === pendingTask.taskId)
+      const isPermittedAutoAssignmentType =
+        targetTask?.autoAssignmentType === AutoAssignmentTypeEnum.all ||
+        targetTask?.autoAssignmentType === pendingTask.projectPropertyType
+      if (!isPermittedAutoAssignmentType) {
+        continue
+      }
+
+      if (task.license_type) {
+        const state = await this.prismaService.states.findFirst({ where: { geoId: project.stateId } })
+        if (!state) continue // TODO: 관리자에게 알람?
+
+        const userLicenses = await this.prismaService.userLicense.findMany({
+          where: {
+            type: task.license_type,
+            abbreviation: state.abbreviation,
+            userId: user.id,
+          },
+        })
+        // 해당 라이센스가 없다면 다음 태스크 순회
+        if (!userLicenses.length) continue
+      }
+
+      // 할당한다.
+      const assignedTaskEntity = this.assignedTaskMapper.toDomain(pendingTask)
+      await assignedTaskEntity.assign(user, this.orderModificationValidator) // OK?
+      await this.assignedTaskRepo.update(assignedTaskEntity)
+      return true
+    }
+    return false
   }
 }
