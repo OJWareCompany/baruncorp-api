6d743995f17950fa29abcc95ab465dbba07e25aa
김효민 <hyomin@gimhyomins-MacBook-Pro.local>
2024-04-10 20:48:26 +0900

    ✨ feat: rate limit

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { ConfigModule } from '@nestjs/config'
 import { ValidationPipe } from '@nestjs/common'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { rateLimit } from 'express-rate-limit'
 import cookieParser from 'cookie-parser'
 import { AppModule } from './app.module'
 
@@ -12,6 +13,12 @@ const { APP_PORT } = process.env
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  app.use(
+    rateLimit({
+      windowMs: Number(1000),
+      max: Number(30),
+    }),
+  )
   app.enableCors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
