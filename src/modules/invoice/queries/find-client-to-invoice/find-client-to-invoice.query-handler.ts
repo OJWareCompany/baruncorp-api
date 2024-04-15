1789bf1e117320f11f193dc5a54a98ab0c53d0e5
김효민 <hyomin@gimhyomins-MacBook-Pro.local>
2024-04-15 19:11:28 +0900

    🐞 fix: modified standard of including jobs to invoice

--- src/modules/invoice/queries/find-client-to-invoice/find-client-to-invoice.query-handler.ts
+++ src/modules/invoice/queries/find-client-to-invoice/find-client-to-invoice.query-handler.ts
@@ -16,14 +16,12 @@ export class FindClientToInvoiceQueryHandler implements IQueryHandler {
   async execute(query: FindClientToInvoiceQuery): Promise<any> {
     // AND created_at < DATE_FORMAT(CURDATE(), '%Y-%m-01')
     const jobs = (await this.prismaService.$queryRaw`
-    SELECT oj.id, oj.client_organization_id, oj.created_at
-    FROM ordered_jobs oj
-    INNER JOIN ordered_services os ON oj.id = os.job_id
-    WHERE oj.job_status IN('Completed','Canceled','Sent To Client','Canceled (Invoice)')
-    AND oj.invoice_id IS NULL
-    AND os.status IN('Completed', 'Canceled (Invoice)')
-    GROUP BY client_organization_id, DATE_FORMAT(created_at, '%Y-%m')
-    ORDER BY created_at DESC;
+    SELECT id, client_organization_id, created_at
+    FROM ordered_jobs
+    WHERE job_status IN('Sent To Client','Canceled (Invoice)')
+    AND invoice_id IS NULL
+    GROUP BY client_organization_id, DATE_FORMAT(date_sent_to_client, '%Y-%m')
+    ORDER BY date_sent_to_client DESC;
     `) as {
       id: string
       client_organization_id: string
