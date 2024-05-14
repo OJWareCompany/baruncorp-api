e29afa0ba21d80f4061a93984dd5f057e488612a
김효민 <hyomin@gimhyomins-MacBook-Pro.local>
2024-05-14 20:24:03 +0900

    🦄 refactor: invoice mapper
    currentCc 타입 좀더 명확하게 수정

--- src/modules/invoice/invoice.mapper.ts
+++ src/modules/invoice/invoice.mapper.ts
@@ -16,12 +16,8 @@ import { IssuedByUserName } from './domain/value-objects/issued-by-user-name.val
 export class InvoiceMapper implements Mapper<InvoiceEntity, InvoiceModel, InvoiceResponseDto> {
   toPersistence(entity: InvoiceEntity): InvoiceModel {
     const props = entity.getProps()
-
-    const currentCc = props.currentCc
-      .filter((cc) => !!cc.email.length)
-      .map((cc) => cc.email)
-      .toString()
-
+    const currentCcForModel: string | null =
+      props.currentCc.map((cc) => cc.email).toString() === '' ? null : props.currentCc.map((cc) => cc.email).toString()
     const record: InvoiceModel = {
       id: props.id,
       status: props.status,
@@ -42,17 +38,14 @@ export class InvoiceMapper implements Mapper<InvoiceEntity, InvoiceModel, Invoic
       issuedAt: props.issuedAt,
       amountPaid: new Decimal(props.amountPaid),
       appliedCredit: new Decimal(props.appliedCredit),
-      currentCc: !!currentCc.length ? currentCc : null,
+      currentCc: currentCcForModel,
       invoiceIssueHistories: props.invoiceIssueHistories.map((history) => {
-        const cc = history.cc
-          .filter((cc) => !!cc.email.length)
-          .map((cc) => cc.email)
-          .toString()
-
+        const historyCc: string | null =
+          history.cc.map((cc) => cc.email).toString() === '' ? null : history.cc.map((cc) => cc.email).toString()
         return {
           invoiceId: history.invoiceId,
           to: history.to.value,
-          cc: !!cc.length ? cc : null,
+          cc: historyCc,
           issuedAt: history.issuedAt,
           issuedByUserId: history.issuedByUserId.value,
           issuedByUserName: history.issuedByUserName.value,
