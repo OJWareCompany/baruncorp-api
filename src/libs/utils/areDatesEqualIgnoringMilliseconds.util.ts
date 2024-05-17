2148cf69ded93e6fa981895153b250ee789bd7fd
김효민 <hyomin@gimhyomins-MacBook-Pro.local>
2024-05-17 18:56:53 +0900

    🐳 chore: added utill areDatesEqualIgnoringMilliseconds

---
+++ src/libs/utils/areDatesEqualIgnoringMilliseconds.util.ts
@@ -0,0 +1,10 @@
+export function areDatesEqualIgnoringMilliseconds(date1: Date, date2: Date): boolean {
+  return (
+    date1.getFullYear() === date2.getFullYear() &&
+    date1.getMonth() === date2.getMonth() &&
+    date1.getDate() === date2.getDate() &&
+    date1.getHours() === date2.getHours() &&
+    date1.getMinutes() === date2.getMinutes() &&
+    date1.getSeconds() === date2.getSeconds()
+  )
+}
