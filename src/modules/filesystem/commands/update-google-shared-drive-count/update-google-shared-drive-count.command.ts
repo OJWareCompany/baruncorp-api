eeb254fb53cacd31e714197c839436d501cd87df
ojsangwon <sangwon@ojware.com>
2024-06-05 14:41:20 +0900

    🐳 chore: something

--- src/modules/filesystem/commands/update-google-shared-drive-count/update-google-shared-drive-count.command.ts
+++
@@ -1,9 +0,0 @@
-import { initialize } from '../../../../libs/utils/constructor-initializer'
-
-export class UpdateGoogleSharedDriveCountCommand {
-  readonly jobFolderId: string
-  readonly count: number
-  constructor(props: UpdateGoogleSharedDriveCountCommand) {
-    initialize(this, props)
-  }
-}
--- src/modules/filesystem/filesystem.module.ts
+++ src/modules/filesystem/filesystem.module.ts
@@ -7,29 +7,14 @@ import { CreateGoogleJobNoteFolderService } from './commands/create-google-job-n
 import { GoogleJobNoteFolderMapper } from './google-job-note-folder.mapper'
 import { GOOGLE_JOB_NOTE_FOLDER_REPOSITORY } from './filesystem.di-token'
 import { GoogleJobNoteFolderRepository } from './database/google-job-note-folder.repository'
-import { FindNonCountedJobFoldersHttpController } from './queries/find-non-counted-job-folders/find-non-counted-job-folders.http.controller'
-import { FindNonCountedJobFoldersQueryHandler } from './queries/find-non-counted-job-folders/find-non-counted-job-folders.query-handler'
-import { UpdateGoogleSharedDriveCountService } from './commands/update-google-shared-drive-count/update-google-shared-drive-count.service'
-import { UpdateGoogleSharedDriveCountHttpController } from './commands/update-google-shared-drive-count/update-google-shared-drive-count.http.controller'
 import { FilesystemDomainService } from './domain/domain-service/filesystem.domain-service'
 import { GoogleAhjNoteFolderDomainService } from './domain/domain-service/google-ahj-note-folder.domain-service'
 import { CreateGoogleAhjNoteFolderHttpController } from './commands/create-google-ahj-note-folder/create-google-ahj-note-folder.http.controller'
 import { CreateGoogleAhjNoteFolderService } from './commands/create-google-ahj-note-folder/create-google-ahj-note-folder.service'
 
-const httpControllers = [
-  CreateGoogleJobNoteFolderHttpController,
-  CreateGoogleAhjNoteFolderHttpController,
-  FindNonCountedJobFoldersHttpController,
-  UpdateGoogleSharedDriveCountHttpController,
-]
+const httpControllers = [CreateGoogleJobNoteFolderHttpController, CreateGoogleAhjNoteFolderHttpController]
 
-const commandHandlers: Provider[] = [
-  CreateGoogleJobNoteFolderService,
-  CreateGoogleAhjNoteFolderService,
-  UpdateGoogleSharedDriveCountService,
-]
-
-const queryHandlers: Provider[] = [FindNonCountedJobFoldersQueryHandler]
+const commandHandlers: Provider[] = [CreateGoogleJobNoteFolderService, CreateGoogleAhjNoteFolderService]
 
 const repositories: Provider[] = [
   {
@@ -49,7 +34,6 @@ const mappers: Provider[] = [GoogleJobNoteFolderMapper]
     ...commandHandlers,
     ...mappers,
     ...repositories,
-    ...queryHandlers,
   ],
   controllers: [...httpControllers],
   exports: [FilesystemApiService, FilesystemDomainService, GoogleAhjNoteFolderDomainService],
